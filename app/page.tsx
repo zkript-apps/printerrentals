674da017c28e50661b4664cac5892df884dd3969
Alkaejah <richard.delapena19@gmail.com>
2024-04-25 09:33:13 +0800

    feat(web): Created features Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import FeatureSection from "@/common/components/Layout/Feature";
 import Footer from "@/common/components/Layout/Footer";
 import { APP_NAME } from "@/common/constants";
 import Home from "@/module/Home";
@@ -11,8 +12,9 @@ export const metadata: Metadata = {
 const HomePage = () => {
   return (
     <>
-      <Home />
-      <Footer />
+      {/* <Home /> */}
+      <FeatureSection/>
+      {/* <Footer /> */}
     </>
   );
 };
