1be980d3c026d13d8ff2c17be71757224e6e246c
Andal <arjayandal93@gmail.com>
2024-04-25 10:46:42 +0800

    feat(web): Added Hero Section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,18 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
+        
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          'auto': 'auto',
+        'cover': 'cover',
+        'contain': 'contain',
       },
     },
   },
   plugins: [],
 };
+
+
 export default config;
