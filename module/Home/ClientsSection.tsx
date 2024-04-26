aba4a451e35dd88aa0d4ee26a905aae59ebc9fa0
Dyayy026 <Dyayy026@users.noreply.github.com>
2024-04-26 05:28:44 +0000

    Prettified Code!

--- module/Home/ClientsSection.tsx
+++ module/Home/ClientsSection.tsx
@@ -8,13 +8,21 @@ const ClientsSection = () => {
         SOME OF OUR SATISFIED CLIENTS
       </Typography>
       <div className="md:flex justify-center gap-6 space-y-4 mx-auto">
-        <img src="/ampc.png" className="md:w-1/3 max-w-xs h-28 mx-auto my-auto" alt="ampc logo" />
+        <img
+          src="/ampc.png"
+          className="md:w-1/3 max-w-xs h-28 mx-auto my-auto"
+          alt="ampc logo"
+        />
         <img
           src="/fiberhome.png"
           className="md:w-1/3 max-w-xs h-30 mx-auto pb-6"
           alt="fiber logo"
         />
-        <img src="/banko.png" className="md:w-1/3 max-w-xs h-28 mx-auto pt-2" alt="banko logo" />
+        <img
+          src="/banko.png"
+          className="md:w-1/3 max-w-xs h-28 mx-auto pt-2"
+          alt="banko logo"
+        />
       </div>
     </div>
   );
