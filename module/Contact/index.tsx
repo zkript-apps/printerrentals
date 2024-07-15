d023b3742c6a7d9779abd729924942a6bab9da5d
kinnethjavier <kinnethandrew9@gmail.com>
2024-07-15 13:33:52 +0800

    refactor(web): Removed width wrapper margin x

--- module/Contact/index.tsx
+++ module/Contact/index.tsx
@@ -4,7 +4,7 @@ import MoreInformation from "./MoreInformation";
 
 const Contact = () => {
   return (
-    <div className="mx-4 md:mx-10 lg:mx-20 mt-10 md:mt-14 lg:mt-20">
+    <div className="mx-4 md:mx-10 2xl:mx-auto mt-10 md:mt-14 lg:mt-20">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
         <ContactForm />
         <MoreInformation />
